4f2d809ad356cf4095e22c133249961999b94562
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-03-18 18:14:01 +0100

    1

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,11 @@ document.getElementById("currentyear").textContent = currentYear;
 
 // Dynamically set the last modified date
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
