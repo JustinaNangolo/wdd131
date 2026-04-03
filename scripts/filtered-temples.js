06cb2ddaeab9bb7b35df4d4d6f83f17e718efca7
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-04-03 21:39:05 +0200

    1

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -23,7 +23,7 @@ const temples = [
         dedicated: "2005, August, 7",
         area: 11500,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+            "images/aba-nigeria.webp"
     },
     {
         templeName: "Manti Utah",
@@ -31,7 +31,7 @@ const temples = [
         dedicated: "1888, May, 21",
         area: 74792,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+            "images/manti.webp"
     },
     {
         templeName: "Payson Utah",
@@ -39,7 +39,7 @@ const temples = [
         dedicated: "2015, June, 7",
         area: 96630,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+            "images/payson-utah-.webp"
     },
     {
         templeName: "Yigo Guam",
@@ -47,7 +47,7 @@ const temples = [
         dedicated: "2020, May, 2",
         area: 6861,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+            "images/yigo_guam.webp"
     },
     {
         templeName: "Washington D.C.",
@@ -55,7 +55,7 @@ const temples = [
         dedicated: "1974, November, 19",
         area: 156558,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+            "images/washington_dc_.webp"
     },
     {
         templeName: "Lima Perú",
@@ -63,7 +63,7 @@ const temples = [
         dedicated: "1986, January, 10",
         area: 9600,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+            "images/lima-peru.webp"
     },
     {
         templeName: "Mexico City Mexico",
@@ -71,7 +71,7 @@ const temples = [
         dedicated: "1983, December, 2",
         area: 116642,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+            "images/mexico-city-.webp"
     },
     {
         templeName: "Sapporo Japan",
@@ -79,7 +79,7 @@ const temples = [
         dedicated: "2016, June, 5", 
         area: 107639,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/sapporo-japan-temple/sapporo-japan-temple-3374-main.jpg"
+            "images/sapporo-japan.webp"
         
     },
 
@@ -89,7 +89,7 @@ const temples = [
         dedicated: "2023, October, 22",   
         area: 48525,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/bangkok-thailand-temple/bangkok-thailand-temple-40055.jpg"
+            "images/bangkok-thailand.webp"
     },
 
     {
@@ -98,7 +98,7 @@ const temples = [
         dedicated: "2022, October, 31",  
         area: 29000,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-benemerito-mexico-temple/mexico-city-benemerito-mexico-temple-56024-main.jpg"
+            "images/mexico-city-benemerito-.webp"
     },
 
     {
@@ -107,7 +107,7 @@ const temples = [
         dedicated: "2025, October, 25",  
         area: 9500,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/cape-town-south-africa-temple/cape-town-south-africa-temple-64609-main.jpg"
+            "images/cape-town-south-africa-.webp"
     },
 
     {
@@ -116,7 +116,7 @@ const temples = [
         dedicated: "1955, September, 9",
         area: 35546,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/bern-switzerland-temple/bern-switzerland-temple-54641-main.jpg"
+            "images/bern-switzerland.webp"
     }
     
     // Add more temple objects here...
