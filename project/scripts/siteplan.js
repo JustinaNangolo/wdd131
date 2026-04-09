62b8ca649728a3e44555bdc83f4a61146579b5da
yourusernameJustinaNangolo <jtnangoloh@gmail.com>
2026-04-09 19:55:54 +0200

    1

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Dynamically set the last modified date
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
